f65b4703caed1d4e4e4219853555130cc508bb95
azakimi123 <aaron.zakimi@gmail.com>
2020-06-17 13:33:22 -0600

    initial commit

---
+++ src/components/TopicBrowser/TopicBrowser.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class TopicBrowser extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hello World</p>
+            </div>
+        )
+    }
+}
+
+export default TopicBrowser;
